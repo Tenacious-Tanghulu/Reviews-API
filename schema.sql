b9137c7235aa8d9ce5d9cf126fc24792b239f434
Evans Ding <evansding@gmail.com>
2021-06-22 20:01:11 -0700

    updated schema and database

--- schema.sql
+++ schema.sql
@@ -2,19 +2,19 @@
 -- Globals
 -- ---
 
--- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET SQL_MODE=NO_AUTO_VALUE_ON_ZERO;
 -- SET FOREIGN_KEY_CHECKS=0;
 
 -- ---
 -- Table 'Product'
 --
 -- ---
-CREATE DATABASE Products;
 
-DROP TABLE IF EXISTS "Product";
 
-CREATE TABLE "Product" (
-  "id_Product" serial PRIMARY KEY ,
+DROP TABLE IF EXISTS Product;
+
+CREATE TABLE Product (
+  id_Product serial PRIMARY KEY
 );
 
 -- ---
@@ -22,22 +22,22 @@ CREATE TABLE "Product" (
 --
 -- ---
 
-DROP TABLE IF EXISTS "Review";
+DROP TABLE IF EXISTS Review;
 
-CREATE TABLE "Review" (
-  "id_Review" serial PRIMARY KEY,
-  "Rating" INT NOT NULL,
-  "Summary" VARCHAR NOT NULL,
-  "Recommend" BOOLEAN NOT NULL,
-  "Response" TEXT NOT NULL,
-  "Body" TEXT  NOT NULL,
-  "Date" DATETIME  NOT NULL,
-  "Reviewer_name" VARCHAR(50) NOT NULL,
-  "Helpfulness" TINYINT NOT NULL,
-  "id_Product" INT NOT NULL,
-  "Email" VARCHAR (255) UNIQUE NOT NULL,
-  FOREIGN KEY ("id_Product")
-  REFERENCES "Product" ("id_Product")
+CREATE TABLE Review (
+  id_Review serial PRIMARY KEY,
+  Rating INT NOT NULL,
+  Summary VARCHAR NOT NULL,
+  Recommend BOOLEAN NOT NULL,
+  Response TEXT NOT NULL,
+  Body TEXT  NOT NULL,
+  Date VARCHAR(50) NOT NULL,
+  Reviewer_name VARCHAR(50) NOT NULL,
+  Helpfulness INT NOT NULL,
+  id_Product INT NOT NULL,
+  Email VARCHAR (255) UNIQUE NOT NULL,
+  FOREIGN KEY (id_Product)
+  REFERENCES Product (id_Product)
 );
 
 -- ---
@@ -45,30 +45,44 @@ CREATE TABLE "Review" (
 --
 -- ---
 
-DROP TABLE IF EXISTS "Photos";
+DROP TABLE IF EXISTS Photos;
 
-CREATE TABLE "Photos" (
-  "id_Photos" serial PRIMARY KEY,
-  "id_Review" INT NOT NULL,
-  "url" TEXT NOT NULL,
-  FOREIGN KEY ("id_Review")
-  REFERENCES "Review" ("id_Review")
+CREATE TABLE Photos (
+  id_Photos serial PRIMARY KEY,
+  id_Review INT NOT NULL,
+  url TEXT NOT NULL,
+  FOREIGN KEY (id_Review)
+  REFERENCES Review (id_Review)
 );
+
+-- ---
+-- Table 'Characteristics'
+--
+-- ---
+
+DROP TABLE IF EXISTS Characteristics;
+
+CREATE TABLE Characteristics (
+  id_Characteristics serial PRIMARY KEY,
+  Description VARCHAR NOT NULL,
+  Value VARCHAR(50) NOT NULL
+);
+
 -- ---
 -- Table 'Product Characteristic'
 --
 -- ---
 
-DROP TABLE IF EXISTS `Product Characteristic`;
+DROP TABLE IF EXISTS Product_Characteristic;
 
-CREATE TABLE `Product Characteristic` (
-  `id_Product_Characteristic` serial PRIMARY KEY,
-  `id_Product` INT NOT NULL,
-  `id_Characteristics` INT NOT NULL,
-  FOREIGN KEY ("id_Product")
-  REFERENCES "Product" ("id_Product"),
-  FOREIGN KEY ("id_Characteristics")
-  REFERENCES "Characteristics" ("id_Characteristics")
+CREATE TABLE Product_Characteristic (
+  id_Product_Characteristic serial PRIMARY KEY,
+  id_Product INT NOT NULL,
+  id_Characteristics INT NOT NULL,
+  FOREIGN KEY (id_Product)
+  REFERENCES Product (id_Product),
+  FOREIGN KEY (id_Characteristics)
+  REFERENCES Characteristics (id_Characteristics)
 );
 
 
@@ -77,56 +91,45 @@ CREATE TABLE `Product Characteristic` (
 --
 -- ---
 
-DROP TABLE IF EXISTS `Characteristic Review`;
+DROP TABLE IF EXISTS Characteristic_Review;
 
-CREATE TABLE `Characteristic Review` (
-  `id_Characteristic Review` serial PRIMARY KEY,
-  `id_Review` INT NOT NULL,
-  `id_Characteristics` INT NOT NULL,
-  FOREIGN KEY ("id_Review")
-  REFERENCES "Review" ("id_Review"),
-  FOREIGN KEY ("id_Characteristics")
-  REFERENCES "Characteristics" ("id_Characteristics")
-);
--- ---
--- Table 'Characteristics'
---
--- ---
-
-DROP TABLE IF EXISTS "Characteristics";
-
-CREATE TABLE "Characteristics" (
-  "id_Characteristic" serial PRIMARY KEY,
-  "Description" VARCHAR NOT NULL,
-  "Value" VARCHAR(50) NOT NULL,
+CREATE TABLE Characteristic_Review (
+  id_Characteristic_Review serial PRIMARY KEY,
+  id_Review INT NOT NULL,
+  id_Characteristics INT NOT NULL,
+  Score VARCHAR(50) NOT NULL,
+  FOREIGN KEY (id_Review)
+  REFERENCES Review (id_Review),
+  FOREIGN KEY (id_Characteristics)
+  REFERENCES Characteristics (id_Characteristics)
 );
 
 -- ---
 -- Foreign Keys
 -- ---
 
--- ALTER TABLE "Review" ADD FOREIGN KEY (id_Product) REFERENCES "Product" ("id");
--- ALTER TABLE "Photos" ADD FOREIGN KEY (id_Review) REFERENCES "Review" ("id");
--- ALTER TABLE "Characteristics" ADD FOREIGN KEY (id_Review) REFERENCES "Review" ("id");
+-- ALTER TABLE Review ADD FOREIGN KEY (id_Product) REFERENCES Product (id);
+-- ALTER TABLE Photos ADD FOREIGN KEY (id_Review) REFERENCES Review (id);
+-- ALTER TABLE Characteristics ADD FOREIGN KEY (id_Review) REFERENCES Review (id);
 
 -- ---
 -- Table Properties
 -- ---
 
--- ALTER TABLE "Product" ENGINE=InnoDB NOT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE "Review" ENGINE=InnoDB NOT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE "Photos" ENGINE=InnoDB NOT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE "Characteristics" ENGINE=InnoDB NOT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE Product ENGINE=InnoDB NOT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE Review ENGINE=InnoDB NOT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE Photos ENGINE=InnoDB NOT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE Characteristics ENGINE=InnoDB NOT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ---
 -- Test Data
 -- ---
 
--- INSERT INTO "Product" ("id") VALUES
+-- INSERT INTO Product (id) VALUES
 -- ('');
--- INSERT INTO "Review" ("id","Rating","Summary","Recommend","Response","Body","Date","Reviewer_name","Helpfulness","id_Product","Email") VALUES
+-- INSERT INTO Review (id,Rating,Summary,Recommend,Response,Body,Date,Reviewer_name,Helpfulness,id_Product,Email) VALUES
 -- ('','','','','','','','','','','');
--- INSERT INTO "Photos" ("id","id_Review","url") VALUES
+-- INSERT INTO Photos (id,id_Review,url) VALUES
 -- ('','','');
--- INSERT INTO "Characteristics" ("id","Description","Value","id_Review") VALUES
+-- INSERT INTO Characteristics (id,Description,Value,id_Review) VALUES
 -- ('','','','');
