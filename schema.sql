f77fc5cc93f3f74a4ab2728ab346e1aa486841c7
Evans Ding <evansding@gmail.com>
2021-06-22 17:53:58 -0700

    updated schema

--- schema.sql
+++ schema.sql
@@ -9,14 +9,12 @@
 -- Table 'Product'
 --
 -- ---
+CREATE DATABASE Products;
 
-DROP TABLE IF EXISTS `Product`;
+DROP TABLE IF EXISTS "Product";
 
-CREATE TABLE `Product` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `Page` INTEGER NULL DEFAULT NULL,
-  `Count` INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE "Product" (
+  "id_Product" serial PRIMARY KEY ,
 );
 
 -- ---
@@ -24,21 +22,22 @@ CREATE TABLE `Product` (
 --
 -- ---
 
-DROP TABLE IF EXISTS `Review`;
+DROP TABLE IF EXISTS "Review";
 
-CREATE TABLE `Review` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `Rating` INTEGER NULL DEFAULT NULL,
-  `Summary` VARCHAR NULL DEFAULT NULL,
-  `Recommend` BINARY NULL DEFAULT NULL,
-  `Response` MEDIUMTEXT NULL DEFAULT NULL,
-  `Body` VARCHAR NULL DEFAULT NULL,
-  `Date` VARCHAR NULL DEFAULT NULL,
-  `Reviewer_name` INTEGER NULL DEFAULT NULL,
-  `Helpfulness` TINYINT NULL DEFAULT NULL,
-  `id_Product` INTEGER NULL DEFAULT NULL,
-  `id_User` INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE "Review" (
+  "id_Review" serial PRIMARY KEY,
+  "Rating" INT NOT NULL,
+  "Summary" VARCHAR NOT NULL,
+  "Recommend" BOOLEAN NOT NULL,
+  "Response" TEXT NOT NULL,
+  "Body" TEXT  NOT NULL,
+  "Date" DATETIME  NOT NULL,
+  "Reviewer_name" VARCHAR(50) NOT NULL,
+  "Helpfulness" TINYINT NOT NULL,
+  "id_Product" INT NOT NULL,
+  "Email" VARCHAR (255) UNIQUE NOT NULL,
+  FOREIGN KEY ("id_Product")
+  REFERENCES "Product" ("id_Product")
 );
 
 -- ---
@@ -46,182 +45,88 @@ CREATE TABLE `Review` (
 --
 -- ---
 
-DROP TABLE IF EXISTS `Photos`;
+DROP TABLE IF EXISTS "Photos";
 
-CREATE TABLE `Photos` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `id_Review` INTEGER NULL DEFAULT NULL,
-  `url` MEDIUMTEXT NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE "Photos" (
+  "id_Photos" serial PRIMARY KEY,
+  "id_Review" INT NOT NULL,
+  "url" TEXT NOT NULL,
+  FOREIGN KEY ("id_Review")
+  REFERENCES "Review" ("id_Review")
 );
-
 -- ---
--- Table 'Ratings'
+-- Table 'Product Characteristic'
 --
 -- ---
 
-DROP TABLE IF EXISTS `Ratings`;
+DROP TABLE IF EXISTS `Product Characteristic`;
 
-CREATE TABLE `Ratings` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `id_Product` INTEGER NULL DEFAULT NULL,
-  `id_Number` INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `Product Characteristic` (
+  `id_Product_Characteristic` serial PRIMARY KEY,
+  `id_Product` INT NOT NULL,
+  `id_Characteristics` INT NOT NULL,
+  FOREIGN KEY ("id_Product")
+  REFERENCES "Product" ("id_Product"),
+  FOREIGN KEY ("id_Characteristics")
+  REFERENCES "Characteristics" ("id_Characteristics")
 );
 
+
 -- ---
--- Table 'Recommended Items'
+-- Table 'Characteristic Review'
 --
 -- ---
 
-DROP TABLE IF EXISTS `Recommended Items`;
+DROP TABLE IF EXISTS `Characteristic Review`;
 
-CREATE TABLE `Recommended Items` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `id_Product` INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `Characteristic Review` (
+  `id_Characteristic Review` serial PRIMARY KEY,
+  `id_Review` INT NOT NULL,
+  `id_Characteristics` INT NOT NULL,
+  FOREIGN KEY ("id_Review")
+  REFERENCES "Review" ("id_Review"),
+  FOREIGN KEY ("id_Characteristics")
+  REFERENCES "Characteristics" ("id_Characteristics")
 );
-
 -- ---
 -- Table 'Characteristics'
 --
 -- ---
 
-DROP TABLE IF EXISTS `Characteristics`;
+DROP TABLE IF EXISTS "Characteristics";
 
-CREATE TABLE `Characteristics` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `Current_Size` VARCHAR NULL DEFAULT NULL,
-  `Current_Comfort` VARCHAR NULL DEFAULT NULL,
-  `Current Width` VARCHAR NULL DEFAULT NULL,
-  `id_Product` INTEGER NULL DEFAULT NULL,
-  `id_Review` INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
-
--- ---
--- Table 'Size'
---
--- ---
-
-DROP TABLE IF EXISTS `Size`;
-
-CREATE TABLE `Size` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `Value` VARCHAR NULL DEFAULT NULL,
-  `id_Characteristics` INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
-
--- ---
--- Table 'Width'
---
--- ---
-
-DROP TABLE IF EXISTS `Width`;
-
-CREATE TABLE `Width` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `Value` VARCHAR NULL DEFAULT NULL,
-  `id_Characteristics` INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
-
--- ---
--- Table 'Comfort'
---
--- ---
-
-DROP TABLE IF EXISTS `Comfort`;
-
-CREATE TABLE `Comfort` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `Value` VARCHAR NULL DEFAULT NULL,
-  `id_Characteristics` INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
-
--- ---
--- Table 'User'
---
--- ---
-
-DROP TABLE IF EXISTS `User`;
-
-CREATE TABLE `User` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `Email` VARCHAR NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
-
--- ---
--- Table 'Number'
---
--- ---
-
-DROP TABLE IF EXISTS `Number`;
-
-CREATE TABLE `Number` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `Rate` TINYINT NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE "Characteristics" (
+  "id_Characteristic" serial PRIMARY KEY,
+  "Description" VARCHAR NOT NULL,
+  "Value" VARCHAR(50) NOT NULL,
 );
 
 -- ---
 -- Foreign Keys
 -- ---
 
-ALTER TABLE `Review` ADD FOREIGN KEY (id_Product) REFERENCES `Product` (`id`);
-ALTER TABLE `Review` ADD FOREIGN KEY (id_User) REFERENCES `User` (`id`);
-ALTER TABLE `Photos` ADD FOREIGN KEY (id_Review) REFERENCES `Review` (`id`);
-ALTER TABLE `Ratings` ADD FOREIGN KEY (id_Product) REFERENCES `Product` (`id`);
-ALTER TABLE `Ratings` ADD FOREIGN KEY (id_Number) REFERENCES `Number` (`id`);
-ALTER TABLE `Recommended Items` ADD FOREIGN KEY (id_Product) REFERENCES `Product` (`id`);
-ALTER TABLE `Characteristics` ADD FOREIGN KEY (id_Product) REFERENCES `Product` (`id`);
-ALTER TABLE `Characteristics` ADD FOREIGN KEY (id_Review) REFERENCES `Review` (`id`);
-ALTER TABLE `Size` ADD FOREIGN KEY (id_Characteristics) REFERENCES `Characteristics` (`id`);
-ALTER TABLE `Width` ADD FOREIGN KEY (id_Characteristics) REFERENCES `Characteristics` (`id`);
-ALTER TABLE `Comfort` ADD FOREIGN KEY (id_Characteristics) REFERENCES `Characteristics` (`id`);
+-- ALTER TABLE "Review" ADD FOREIGN KEY (id_Product) REFERENCES "Product" ("id");
+-- ALTER TABLE "Photos" ADD FOREIGN KEY (id_Review) REFERENCES "Review" ("id");
+-- ALTER TABLE "Characteristics" ADD FOREIGN KEY (id_Review) REFERENCES "Review" ("id");
 
 -- ---
 -- Table Properties
 -- ---
 
--- ALTER TABLE `Product` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Review` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Photos` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Ratings` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Recommended Items` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Characteristics` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Size` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Width` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Comfort` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `User` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Number` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE "Product" ENGINE=InnoDB NOT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE "Review" ENGINE=InnoDB NOT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE "Photos" ENGINE=InnoDB NOT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE "Characteristics" ENGINE=InnoDB NOT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ---
 -- Test Data
 -- ---
 
--- INSERT INTO `Product` (`id`,`Page`,`Count`) VALUES
--- ('','','');
--- INSERT INTO `Review` (`id`,`Rating`,`Summary`,`Recommend`,`Response`,`Body`,`Date`,`Reviewer_name`,`Helpfulness`,`id_Product`,`id_User`) VALUES
+-- INSERT INTO "Product" ("id") VALUES
+-- ('');
+-- INSERT INTO "Review" ("id","Rating","Summary","Recommend","Response","Body","Date","Reviewer_name","Helpfulness","id_Product","Email") VALUES
 -- ('','','','','','','','','','','');
--- INSERT INTO `Photos` (`id`,`id_Review`,`url`) VALUES
+-- INSERT INTO "Photos" ("id","id_Review","url") VALUES
 -- ('','','');
--- INSERT INTO `Ratings` (`id`,`id_Product`,`id_Number`) VALUES
--- ('','','');
--- INSERT INTO `Recommended Items` (`id`,`id_Product`) VALUES
--- ('','');
--- INSERT INTO `Characteristics` (`id`,`Current_Size`,`Current_Comfort`,`Current Width`,`id_Product`,`id_Review`) VALUES
--- ('','','','','','');
--- INSERT INTO `Size` (`id`,`Value`,`id_Characteristics`) VALUES
--- ('','','');
--- INSERT INTO `Width` (`id`,`Value`,`id_Characteristics`) VALUES
--- ('','','');
--- INSERT INTO `Comfort` (`id`,`Value`,`id_Characteristics`) VALUES
--- ('','','');
--- INSERT INTO `User` (`id`,`Email`) VALUES
--- ('','');
--- INSERT INTO `Number` (`id`,`Rate`) VALUES
--- ('','');
+-- INSERT INTO "Characteristics" ("id","Description","Value","id_Review") VALUES
+-- ('','','','');
